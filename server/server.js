b68071d12f9351f594410b9fb86c1a414e0cacc6
Amanda Gray <floridabelle2021@protonmail.com>
2023-05-25 14:43:14 -0600

    making it better

--- server/server.js
+++ server/server.js
@@ -18,16 +18,17 @@
 
 // app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
 
-
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static('/client/src'));
+app.use(express.static(path.join(__dirname, 'client', 'src')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+
